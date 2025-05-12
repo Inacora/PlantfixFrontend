3d06be7adbe549cdb6b30bb4678193f70e6cef32
Inacora <inacorateresacamposalonso@alumno.ieselrincon.es>
2025-05-12 13:31:35 +0100

    cambios en el sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/shared/header/header.component";
+import { SidebarComponent } from "./components/shared/sidebar/sidebar.component";
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrls: []
 })
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,11 +4,10 @@ import { FormComponent } from "../components/home/form/form.component";
 import { AnimatedIconsComponent } from '../components/home/animated-icons/animated-icons.component';
 import { HeroTextComponent } from "../components/home/hero-text/hero-text.component";
 import { ProductItemComponent } from '../components/home/product-item/product-item.component';
-import { SidebarComponent } from "../components/shared/sidebar/sidebar.component";
 
 @Component({
   selector: 'app-home',
-  imports: [HeroComponent, FormComponent, AnimatedIconsComponent, HeroTextComponent, ProductItemComponent, SidebarComponent],
+  imports: [HeroComponent, FormComponent, AnimatedIconsComponent, HeroTextComponent, ProductItemComponent],
   templateUrl: './home.component.html',
   styleUrls: []
 })
