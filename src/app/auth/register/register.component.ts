409fa9af4ee69edb0b9ecb4e4573a162bd3d29e4
Inacora <inacorateresacamposalonso@alumno.ieselrincon.es>
2025-05-17 20:04:10 +0100

    cosas arregladas

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+
 import { UserService } from '../../services/user.service';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, AbstractControl } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-register',
@@ -12,33 +14,37 @@ import { RouterModule } from '@angular/router';
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
-    userForm = new FormGroup({
-       name: new FormControl(''),
-       email: new FormControl(''),
-       password: new FormControl(''),
-     });
-   
-     constructor(private route: ActivatedRoute, private service: UserService) {}
-   
-     users: any[] = [];
-  
-     ngOnInit() {
-       this.service.getUsers().subscribe(data => {
-         this.users = data as any[];
-       });
-     }  
-   
-     createUser() {
-       const user = {
-        name: this.userForm.get('name')?.value,
-        email: this.userForm.get('email')?.value,
-        password: this.userForm.get('password')?.value,
-       };
-   
-       this.service.createUser(user).subscribe(response => {
-         console.log('User saved successfully', response);
-       }, error => {
-         console.error('Error saving user', error);
-       });
-     }
+  userForm = new FormGroup({
+    name: new FormControl(''),
+    email: new FormControl(''),
+    password: new FormControl(''),
+    confirmPassword: new FormControl(''),
+  });
+
+  constructor(private route: ActivatedRoute, private service: UserService) { }
+
+
+ passwordsMatchValidator(form: AbstractControl) {
+    const password = form.get('password')?.value;
+    const confirmPassword = form.get('confirmPassword')?.value;
+    return password === confirmPassword ? null : { passwordsMismatch: true };
+  }
+
+  createUser() {
+       if (this.userForm.valid) {
+      const { confirmPassword, ...userData } = this.userForm.value;
+    const user = {
+      name: this.userForm.get('name')?.value,
+      email: this.userForm.get('email')?.value,
+      password: this.userForm.get('password')?.value,
+
+    };
+
+    this.service.createUser(user).subscribe(response => {
+      console.log('User saved successfully', response);
+    }, error => {
+      console.error('Error saving user', error);
+    });
+  }
+}
 }
